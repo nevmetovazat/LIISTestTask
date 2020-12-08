a262bef8e6875017ad13c994936ae1249d72a97d
Nevmetov Azat <nevmetov.azat@gmail.com>
2020-12-08 20:36:35 +0300

    Initial commit

---
+++ booking/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+
+class Room(models.Model):
+    number = models.PositiveIntegerField(null=False, blank=False, verbose_name='Номер комнаты')
+    name = models.CharField(max_length=64, null=False, blank=False, verbose_name='Имя комнаты')
+
+    @property
+    def bookings(self):
+        return self.booking_set.all()
+
+class Booking(models.Model):
+    datetime_from = models.DateTimeField(verbose_name='Начало брони', null=False, blank=False)
+    datetime_to = models.DateTimeField(verbose_name='Конец брони', null=False, blank=False)
+    room = models.ForeignKey('Room', on_delete=models.PROTECT,
+                             verbose_name='Забронированная комната')
+    booked_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  verbose_name='Пользователь, поставивший бронь')
