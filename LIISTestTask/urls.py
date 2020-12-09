6258ba7e17f8170c08c4220537f48df0573d0037
Nevmetov Azat <nevmetov.azat@gmail.com>
2020-12-09 23:20:58 +0300

    Added swagger documentation for api

--- LIISTestTask/urls.py
+++ LIISTestTask/urls.py
@@ -17,9 +17,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="LIIS test task API",
+      default_version='v1',
+      description="Booking API",
+      contact=openapi.Contact(email="nevmetov.azat@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/auth/', include('authentication.urls', namespace='authentication')),
     url(r'^api/booking/', include('booking.urls', namespace='booking')),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
