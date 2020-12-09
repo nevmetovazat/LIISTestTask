77bd27f3b29110e41a1c190060e1104ca1c92f82
Nevmetov Azat <nevmetov.azat@gmail.com>
2020-12-09 19:49:29 +0300

    Added Booking and Room models to admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Room, Booking
 
 # Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    pass
