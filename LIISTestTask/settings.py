c3080734d0ec0b4d3480463d6c625b64c853ffbd
Nevmetov Azat <nevmetov.azat@gmail.com>
2020-12-10 04:06:29 +0300

    Config fix

--- LIISTestTask/settings.py
+++ LIISTestTask/settings.py
@@ -109,7 +109,9 @@ WSGI_APPLICATION = 'LIISTestTask.wsgi.application'
 #     }
 # }
 
-DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+DATABASES = {
+    "default": env.db("DATABASE_URL")
+}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
